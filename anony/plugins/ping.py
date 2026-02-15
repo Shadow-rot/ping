e935cfa264c19bbc816c6893b5022492ae38c269
Shadwoo <looktouhid@gmail.com>
2026-02-15 21:55:27 +0530

    Update ping.py

--- anony/plugins/ping.py
+++ anony/plugins/ping.py
@@ -1,39 +1,33 @@
-# Copyright (c) 2025 AnonymousX1025
-# Licensed under the MIT License.
-# This file is part of AnonXMusic
-
-
-import time
-import psutil
-
-from pyrogram import filters, types
-from anony import app, anon, boot, config, lang
-from anony.helpers import buttons
 from pyrogram import enums
+from pyrogram.types import MessageEntity
 
+emoji_id = 5375125990118793401
+emoji = "😊"
 
+caption_text = f"{emoji} " + m.lang["ping_pong"].format(
+    latency,
+    uptime,
+    psutil.cpu_percent(interval=0),
+    psutil.virtual_memory().percent,
+    psutil.disk_usage("/").percent,
+    await anon.ping(),
+)
 
-@app.on_message(filters.command(["alive", "ping"]) & ~app.bl_users)
-@lang.language()
-async def _ping(_, m: types.Message):
-    start = time.time()
-    sent = await m.reply_text(m.lang["pinging"])
-    get_time = lambda s: (lambda r: (f"{r[-1]}, " if r[-1][:-4] != "0" else "") + ":".join(reversed(r[:-1])))([f"{v}{u}" for v, u in zip([s%60, (s//60)%60, (s//3600)%24, s//86400], ["s", "m", "h", "days"])])
-    uptime = get_time(int(time.time() - boot))
-    latency = round((time.time() - start) * 1000, 2)
-    await sent.edit_media(
+offset = 0
+length = len(emoji.encode("utf-16-le")) // 2
+
+await sent.edit_media(
     media=types.InputMediaPhoto(
         media=config.PING_IMG,
-        caption=m.lang["ping_pong"].format(
-            latency,
-            uptime,
-            psutil.cpu_percent(interval=0),
-            psutil.virtual_memory().percent,
-            psutil.disk_usage("/").percent,
-            await anon.ping(),
-        ),
-        parse_mode=enums.ParseMode.HTML  # ✅ Correct
+        caption=caption_text,
+        caption_entities=[
+            MessageEntity(
+                type=enums.MessageEntityType.CUSTOM_EMOJI,
+                offset=offset,
+                length=length,
+                custom_emoji_id=emoji_id,
+            )
+        ]
     ),
     reply_markup=buttons.ping_markup(m.lang["support"]),
 )
-
