518be48dc16790aa99d97fa411beb6b7b24362c1
Shadwoo <looktouhid@gmail.com>
2026-02-15 22:22:14 +0530

    Update ping.py

--- anony/plugins/ping.py
+++ anony/plugins/ping.py
@@ -1,9 +1,7 @@
 import time
 import psutil
 
-from pyrogram import filters, types, enums
-from pyrogram.types import MessageEntity
-
+from pyrogram import filters, types
 from anony import app, anon, boot, config, lang
 from anony.helpers import buttons
 
@@ -17,40 +15,18 @@ async def _ping(_, m: types.Message):
 
     uptime = int(time.time() - boot)
     latency = round((time.time() - start) * 1000, 2)
+    assistant_ping = await anon.ping()
 
-    assistant_ping = await anon.ping()  # ✅ await inside function
-
-    emoji = "😊"
-    emoji_id = 5375125990118793401
-
-    caption_text = f"{emoji} " + m.lang["ping_pong"].format(
+    caption = m.lang["ping_pong"].format(
         latency,
         uptime,
         psutil.cpu_percent(interval=0),
         psutil.virtual_memory().percent,
         psutil.disk_usage("/").percent,
         assistant_ping,
-        print(repr(m.lang["ping_pong"])),
     )
 
-    from pyrogram import enums
-    from pyrogram.types import MessageEntity
-
-    offset = 0
-    length = len(emoji.encode("utf-16-le")) // 2
-
-    await sent.edit_media(
-        media=types.InputMediaPhoto(
-            media=config.PING_IMG,
-            caption=caption_text,
-            caption_entities=[
-                MessageEntity(
-                    type=enums.MessageEntityType.CUSTOM_EMOJI,
-                    offset=offset,
-                    length=length,
-                    custom_emoji_id=emoji_id,
-                )
-            ],
-        ),
-        reply_markup=buttons.ping_markup(m.lang["support"]),
+    await sent.edit_caption(
+        caption,
+        reply_markup=buttons.ping_markup(m.lang["support"])
     )
