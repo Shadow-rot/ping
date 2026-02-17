03bae5d9d2947768e0efaf6da7076da5de861798
Shadwoo <looktouhid@gmail.com>
2026-02-17 12:41:13 +0530

    Update h.py

--- anony/plugins/h.py
+++ anony/plugins/h.py
@@ -1,31 +1,24 @@
 import asyncio
 from pyrogram import filters
 from pyrogram.errors import FloodWait
-from anony import app
-from anony.core.userbot import ub
+from anony import app, userbot
 
 
 @app.on_message(filters.command("lv", prefixes=["."]))
 async def leave_all(_, message):
-    total_left = 0
-    print(ub.clients)
+    left = 0
 
-    for assistant in ub.clients:
+    for assistant in userbot.clients:
         async for dialog in assistant.get_dialogs():
             try:
-                chat_id = dialog.chat.id
-
-                
-                if chat_id == assistant.me.id:
-                    continue
-
-                await assistant.leave_chat(chat_id)
-                total_left += 1
+                await assistant.leave_chat(dialog.chat.id)
+                left += 1
                 await asyncio.sleep(0.3)
 
             except FloodWait as e:
                 await asyncio.sleep(e.value)
-            except Exception:
+            except:
                 pass
 
-    await message.reply(f"Userbots left `{total_left}` chats.")
+    await message.reply(f"Left from `{left}` chats.")
+
