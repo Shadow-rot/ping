55f198fa62a5c14726b3bf137517a75fa681c1f3
Shadwoo <looktouhid@gmail.com>
2026-02-17 12:29:24 +0530

    Update h.py

--- anony/plugins/h.py
+++ anony/plugins/h.py
@@ -7,17 +7,24 @@ from anony.core.userbot import ub
 
 @app.on_message(filters.command("lv", prefixes=["."]))
 async def leave_all(_, message):
-    left = 0
+    total_left = 0
 
     for assistant in ub.clients:
         async for dialog in assistant.get_dialogs():
             try:
-                await assistant.leave_chat(dialog.chat.id)
-                left += 1
-                await asyncio.sleep(0.2)
+                chat_id = dialog.chat.id
+
+                
+                if chat_id == assistant.me.id:
+                    continue
+
+                await assistant.leave_chat(chat_id)
+                total_left += 1
+                await asyncio.sleep(0.3)
+
             except FloodWait as e:
                 await asyncio.sleep(e.value)
-            except:
+            except Exception:
                 pass
 
-    await message.reply(f"Left from `{left}` chats.")
+    await message.reply(f"Userbots left `{total_left}` chats.")
